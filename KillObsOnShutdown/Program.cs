5cb1dcf32da31cfee32c8aad4fa48bdb017901c7
Lyze <11274700+lyze237@users.noreply.github.com>
2019-07-15 10:22:24 +0200

    added everything

---
+++ KillObsOnShutdown/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using Microsoft.Win32;
+
+namespace KillObsOnShutdown
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            SystemEvents.SessionEnding += SystemEventsOnSessionEnding;
+
+            while (true)
+            {
+                Thread.Sleep(999999999);
+            }
+        }
+
+        private static void SystemEventsOnSessionEnding(object sender, SessionEndingEventArgs e)
+        {
+            SystemEvents.SessionEnding -= SystemEventsOnSessionEnding;
+            e.Cancel = false;
+
+            Process.GetProcessesByName("obs64").ToList().ForEach(p => p.Kill());
+            
+            /*
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "taskkill.exe",
+                    Arguments = "/F /IM obs64.exe",
+                    UseShellExecute = true
+                }
+            };
+
+            process.Start();
+            process.WaitForExit();
+            */
+            
+            Environment.Exit(0);
+        }
+    }
+}
